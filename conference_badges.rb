8d9ece552d2b8e9cdeb876cc816fc1eb3f7cdd4f
Ititi Obidah <iobidah9@gmail.com>
2019-04-10 13:21:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,26 @@
+def badge_maker(name)
+  "Hello, my name is #{name}." 
+end
 
-def badge_maker(name) 
- "Hello, my name is #{name}."
-end 
 def batch_badge_creator(attendees)
   attendees.collect do |name|
-    badge_maker(name)
- end
+badge_maker(name)
+ end 
 end
+
 def assign_rooms(attendees)
-  room= 0
-  attendees.collect do |name|
-    room += 1
- "Hello, #{name}! You'll be assigned to room #{room}!"
-  end 
-end
-def printer(attendees)
-  batch_badge_creator(attendees).each do |value|
-    puts value
-end
-  assign_rooms(attendees).each do |value|
-    puts value
-end 
+room = 0 
+attendees.collect do |name|
+  room += 1
+  "Hello, #{name}! You'll be assigned to room #{room}!"
  end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).collect do |value|
+     puts value
+   end
+    assign_rooms(attendees).collect do |value|
+     puts value
+ end 
+end
